17b89ec62233cc4bef9e7dcf6637e14386ec3778
Jaerock Kwon <jrkwon@umich.edu>
2025-10-14 15:52:38 -0400

    add test code

--- 05/module5.py
+++ 05/module5.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 
 mnist_data = MnistData()
 (_, _), (test_images, test_labels) = mnist_data.get_dataset()
-img = test_images[500]
-label = test_labels[500]
-plt.imshow(img, cmap='gray')
-print(label)
+# get two test images and labels
+test1 = test_images[3]
+test2 = test_images[8]
+label1 = test_labels[3]
+label2 = test_labels[8]
+
+# Let's plot the test images
+plt.subplot(1, 2, 1)
+plt.imshow(test1, cmap='gray')
+plt.title(f"Label: {label1}")
+plt.subplot(1, 2, 2)
+plt.imshow(test2, cmap='gray')
+plt.title(f"Label: {label2}")
 plt.show()
